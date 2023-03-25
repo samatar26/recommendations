9034facf61678bd8328b004ac40677269aef0e05
Samatar <axmedsamatar.dev@gmail.com>
2023-03-25 12:51:11 +0000

    feat: add initial database setup including migrations and orm (#4)
    * feat: add initial database setup including migrations and orm
    
    * feat: add postgres to circleci config
    
    * refactor: use docker rather than circleci orbs

---
+++ recommendations_api/dependencies.py
@@ -0,0 +1,9 @@
+from recommendations_api.db import Session
+
+
+def get_session():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,37 @@
 from recommendations_api.main import app
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from recommendations_api.env import DATABASE_URL
+from recommendations_api.dependencies import get_session
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(autocommit=False, autoflush=False)
+
+
+@pytest.fixture(scope="module")
+def connection():
+    connection = engine.connect()
+    yield connection
+    connection.close()
 
 
 @pytest.fixture()
-def client():
+def session(connection) -> Session:
+    transaction = connection.begin()
+    session = Session(bind=connection)
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+
+
+@pytest.fixture()
+def client(session) -> TestClient:
+    def _get_session_override():
+        yield session
+
+    app.dependency_overrides[get_session] = _get_session_override
     return TestClient(app)
